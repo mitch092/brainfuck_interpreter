cabca5fec6cef2b2f68ccbd2aabab6e70a8b55a7
mitch092 <stevenm7861@gmail.com>
2019-07-09 02:02:04 -0700

    Moved the CMakeLists.txt file and added a few more linew to the conanfile.

--- conanfile.py
+++ conanfile.py
@@ -1,26 +1,32 @@
 from conans import ConanFile, CMake
 
-class glfwTestConan(ConanFile):
-    name = "sfml_template"
+class TheTemplateConan(ConanFile):
+    name = "TheTemplate"
     version = "0.1"
     #license = "<Put the package license here>"
+    #author = "<Put your name here> <And your email here>"
     #url = "<Package recipe repository url here, for issues about the package>"
     description = "This package simply opens a window with SFML. It tests Conan, Cmake and SFML."
+    #topics = ("<Put some tag here>", "<here>", "<and here>")
+
     settings = "os", "compiler", "build_type", "arch"
 	# If this package has configure flags, they go here.
     options = {"shared": [True, False]}
-	# This is where the dependencies are configured.
-    default_options = {"shared": False, "sfml:graphics": True, "sfml:window": True, "sfml:audio": False, "sfml:network": False}
-	# This is where all of the dependencies for the project go.
+    generators = "cmake"
+    exports_sources = "src/*"
+
+    # This is where all of the dependencies for the project go.
     # Others: ["glfw/3.2.1@bincrafters/stable", "sdl2/2.0.9@bincrafters/stable", "glad/0.1.29@bincrafters/stable"] etc.
     requires = "sfml/2.5.1@bincrafters/stable"
-    generators = "cmake"
-    exports_sources = "*"
+
+    # This is where the dependencies are configured.
+    default_options = {"shared": False, "sfml:graphics": True, "sfml:window": True, "sfml:audio": False, "sfml:network": False}
+
 
     def configure_cmake(self):
         cmake = CMake(self)
         #cmake.definitions["SOME_DEFINITION"] = True
-        cmake.configure()
+        cmake.configure(source_folder="src")
         return cmake
 
     def build(self):
@@ -32,4 +38,4 @@ class glfwTestConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["sfml_template"]
+        self.cpp_info.libs = ["TheTemplate"]
